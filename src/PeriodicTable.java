ae00b3a343912a55c5a71d15713f397c112e9630
mcpodo <porteryoungman@gmail.com>
2022-09-21 02:02:01 -0400

    first commit

--- src/PeriodicTable.java
+++ src/PeriodicTable.java
@@ -1,7 +1,7 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+//import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
