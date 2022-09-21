ca2df4d8fa118a9c85b60e1a634d9298a7d4e1a5
mcpodo <porteryoungman@gmail.com>
2022-09-21 03:51:02 -0400

    Overhaul of alignment system, much nicer:
    using separate VBox for left, right, center
    
    works good for sections of periodic table
    
    using alignment with bottom (Possibly temporary... may stay but tweaked)

--- src/PeriodicTable.java
+++ src/PeriodicTable.java
@@ -4,6 +4,7 @@
  * Date Last Modified 9/21/22
  */
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -17,9 +18,14 @@ public class PeriodicTable extends Application{
 		launch(args);
 	}
 	
-	private Button[] btns = new Button[10];
-	private String[] symbols = {"H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne"};
+	private Button[] btns = new Button[36];
+	private String[] symbols = {"H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne", "Na","Mg","Al","Si","P","S","Cl","Ar","K","Ca","Sc","Ti","V","Cr","Mn","Fe","Co","Ni","Cu","Zn","Ga","Ge","As","Se","Br","Kr"};
 	private BorderPane table = new BorderPane();
+	private VBox leftTable = new VBox();
+	private VBox centerTable = new VBox();
+	private VBox rightTable = new VBox();
+
+	
 	
 	private void createButtons() {
 		for (int x=0; x<symbols.length; x++) {
@@ -28,14 +34,14 @@ public class PeriodicTable extends Application{
 		}
 	}
 	
-	private int buildTable(BorderPane row, int index, int rowNum) {
+	private int buildTable(int index, int rowNum) {
 		HBox tempLeft = new HBox();
 		HBox tempCenter = new HBox();
 		HBox tempRight = new HBox();
 		if (rowNum == 1) {
-			row.setLeft(btns[index]);
+			leftTable.getChildren().add(btns[index]);
 			index++;
-			row.setRight(btns[index]);
+			rightTable.getChildren().add(btns[index]);
 			return index;
 		}
 		else if (rowNum >= 2 && rowNum <= 3) {
@@ -45,19 +51,23 @@ public class PeriodicTable extends Application{
 				else
 					tempRight.getChildren().add(btns[x]);
 			}
-			row.setLeft(tempLeft);
-			row.setRight(tempRight);
+			leftTable.getChildren().add(tempLeft);
+			rightTable.getChildren().add(tempRight);
 			return index+8;
 		}
 		else if (rowNum >=4 && rowNum <= 5) {
 			for(int x=index; x<index+18; x++) {
-				if (x<2)
-					row.setLeft(btns[x]);
-				else if (x<12)
-					row.setCenter(btns[x]);
+				if (x<2+index)
+					tempLeft.getChildren().add(btns[x]);
+				else if (x<12+index)
+					tempCenter.getChildren().add(btns[x]);
 				else
-					row.setRight(btns[x]);
+					tempRight.getChildren().add(btns[x]);
 			}
+			leftTable.getChildren().add(tempLeft);
+			centerTable.getChildren().add(tempCenter);
+			rightTable.getChildren().add(tempRight);
+			return index+18;
 		}
 		return index;
 	}
@@ -67,38 +77,35 @@ public class PeriodicTable extends Application{
 		createButtons();
 		//Label text = new Label("Test");
 		VBox pTable = new VBox();
+
+
+		
 		BorderPane row1 = new BorderPane();
 		BorderPane row2 = new BorderPane();
 		BorderPane row3 = new BorderPane();
 		//BorderPane row4 = new BorderPane();
 		
-		buildTable(row1, 0, 1);
-		buildTable(row2, 2, 2);
+		buildTable(0, 1);
+		buildTable(2, 2);
+		buildTable(10,3);
+		buildTable(18,4);
 		
 		
 
-		pTable.getChildren().addAll(row1, row2);
+//		pTable.getChildren().addAll(row1, row2);
 		
 		
 
-//		Button h = new Button("H");
-//		Button li = new Button("Li");
-//		Button na = new Button("Na");
-//		h.resize(100, 100);
+//		lmao.setCenter(pTable);
 		
+		table.setLeft(leftTable);
+		table.setCenter(centerTable);
+		table.setRight(rightTable);
+		leftTable.setAlignment(Pos.BOTTOM_LEFT);
+		centerTable.setAlignment(Pos.BOTTOM_CENTER);
+		rightTable.setAlignment(Pos.BOTTOM_RIGHT);
 		
-		
-		
-//		col1.getChildren().add(h);
-//		col1.getChildren().add(text);
-//		col1.getChildren().add(new Label("d"));
-//		col1.getChildren().add(new Label("f"));
-//		pTable.getChildren().add(col1);
-//		pTable.getChildren().add(new VBox(new Label("g")));
-		lmao.setCenter(pTable);
-		
-		
-		Scene scene = new Scene(lmao,400,400);
+		Scene scene = new Scene(table,900,500);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Periodic Table");
 		primaryStage.show();
